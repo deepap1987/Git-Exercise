a99c8ec8a5d5bf13a43a2956ba1e9897ec9dced7
nirajc1 <81496947+nirajc1@users.noreply.github.com>
2022-10-05 16:06:35 -0400

    add sub method

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -2,14 +2,19 @@ public class Math {
     int a;
     int b;
     int sum;
+    int sub;
 
     public void getSum(int a, int b) {
         System.out.println(sum);
+    }
+    public void getsub(int a, int b) {
+        System.out.println(sub);
     }
 
     public static void main(String[] args) {
         Math m1=new Math();
         m1.getSum(10,20);
+        m1.getsub(30,10);
     }
 
 
