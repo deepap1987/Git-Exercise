52ab348b21078e6ba5fb6ee12b388739dacf3bdf
nirajc1 <81496947+nirajc1@users.noreply.github.com>
2022-10-05 15:57:04 -0400

    Adding New Project

---
+++ src/main/java/Math.java
@@ -0,0 +1,16 @@
+public class Math {
+    int a;
+    int b;
+    int sum;
+
+    public void getSum(int a, int b) {
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        Math m1=new Math();
+        m1.getSum(10,20);
+    }
+
+
+}
